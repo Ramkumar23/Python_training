bdd303898efcb4cc2a81eb0021a93b939aeb5adb
Raaga <raagavendran@gmail.com>
2017-02-02 09:46:33 +0530

    Add files via upload
    ssh by python pexpect

---
+++ sshpx.py
@@ -0,0 +1,11 @@
+from pexpect import pxssh
+import getpass
+s = pxssh.pxssh()
+s.force_password = True
+hostname = raw_input('hostname: ')
+username = raw_input('username: ')
+password = getpass.getpass('password: ')
+s.login (hostname, username, password)
+s.sendline('ls')
+s.prompt()
+print s.before
